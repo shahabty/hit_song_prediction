a65b5d5367ae46a28f4c7779c5e1e31f62f57518
shahabty <nabaviss@eecs.yorku.ca>
2020-04-14 17:14:06 -0400

    progress bar

--- train_random_forest.py
+++ train_random_forest.py
@@ -10,13 +10,15 @@ def main():
     test_set = OneMillion(is_test = True)
     X_train, y_train = shuffle(train_set.data, train_set.labels[:,1])
     X_test, y_test = shuffle(test_set.data, test_set.labels[:,1])
-    print("random forest")
+    print("Random Forest:")
     print(random_forest(X_train, y_train, X_test, y_test))
     return
 
 def random_forest(X_train, y_train, X_test, y_test):
+    print('Training in progress...')
     rf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth = None, max_features='auto')
     rf.fit(X_train, y_train)
+    print('Testing in progress...')
     y_pred = rf.predict(X_test)
     precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='binary')
     cf = confusion_matrix(y_test, y_pred)
--- train_svm.py
+++ train_svm.py
@@ -9,13 +9,15 @@ def main():
     test_set = OneMillion(is_test = True)
     X_train, y_train = shuffle(train_set.data, train_set.labels[:,1])
     X_test, y_test = shuffle(test_set.data, test_set.labels[:,1])
-    print("svm")
+    print("SVM:")
     print(support_vector_machine(X_train, y_train, X_test, y_test))
     return 
 
 def support_vector_machine(X_train, y_train, X_test, y_test):
+    print('Training in progress...')
     svm_clf = svm.SVC(kernel='rbf', degree=10, gamma='auto')
     svm_clf.fit(X_train, y_train)
+    print('Testing in progress...')
     y_pred = svm_clf.predict(X_test)
     precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='binary')
     cf = confusion_matrix(y_test, y_pred)
